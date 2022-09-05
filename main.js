a460cdd546ed57fba952ebfdceed25a845586d18
wannapornd <wannapornd@hellospec.com>
2022-09-06 00:08:20 +0700

    follow on video

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-import "./style.css";
+import "./index.css";
+
+const cards = document.querySelectorAll(".card");
+
+function removeActiveCard() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+}
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    removeActiveCard();
+    card.classList.add("active");
+  });
+});
