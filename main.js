8cac30650958dc2c4f8d9307f56ef30ae207328f
wannapornd <wannapornd@hellospec.com>
2022-09-14 00:18:00 +0700

    Able to vertical slide when click the button

--- main.js
+++ main.js
@@ -67,3 +67,47 @@ function updateProgressStyle() {
     ((activeCircles.length - 1) / (circles.length - 1)) * 100
   }%`;
 }
+
+// double vertical slider
+const sliderContainer = document.getElementById("double-vertical-slider");
+const leftSlide = document.querySelector(".left-slide");
+const rightSlide = document.querySelector(".right-slide");
+const slideRightLength = rightSlide.querySelectorAll("div").length;
+const upBtn = document.querySelector(".up-btn");
+const downBtn = document.querySelector(".down-btn");
+let activeSlideIndx = 0;
+
+// Configure for slide from up to down
+rightSlide.style.top = `-${(slideRightLength - 1) * 100}vh`;
+
+upBtn.addEventListener("click", function () {
+  slider("up");
+});
+downBtn.addEventListener("click", function () {
+  slider("down");
+});
+
+function slider(direction) {
+  const sliderHeight = sliderContainer.clientHeight;
+
+  if (direction === "up") {
+    activeSlideIndx++;
+
+    if (activeSlideIndx > slideRightLength - 1) {
+      activeSlideIndx = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndx--;
+
+    if (activeSlideIndx < 0) {
+      activeSlideIndx = slideRightLength - 1;
+    }
+  }
+
+  leftSlide.style.transform = `translateY(-${
+    activeSlideIndx * sliderHeight
+  }px)`;
+  rightSlide.style.transform = `translateY(${
+    activeSlideIndx * sliderHeight
+  }px)`;
+}
