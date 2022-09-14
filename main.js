de7115b08511bafd65fe7230b0e7ae1e1544a887
wannapornM <86010899+wannapornM@users.noreply.github.com>
2022-09-14 09:35:35 +0700

    Double vertical slider (#3)
    * Change style from css to scss
    
    * Change style of expanding cards to scss
    
    * Change style of progress steps to scss
    
    * Change entrypoint style to scss
    
    * Create layout of vertical slider project
    
    * Able to vertical slide when click the button
    
    * Collapse same variables
    
    Co-authored-by: wannapornd <wannapornd@hellospec.com>

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import "./index.css";
+import "./index.scss";
 
 // expanding-cards
 const cards = document.querySelectorAll(".card");
@@ -66,4 +66,48 @@ function updateProgressStyle() {
   progressLine.style.width = `${
     ((activeCircles.length - 1) / (circles.length - 1)) * 100
   }%`;
+}
+
+// double vertical slider
+const sliderHeight = document.getElementById(
+  "double-vertical-slider"
+).clientHeight;
+const leftSlide = document.querySelector(".left-slide");
+const rightSlide = document.querySelector(".right-slide");
+const slideRightLength = rightSlide.querySelectorAll("div").length;
+const upBtn = document.querySelector(".up-btn");
+const downBtn = document.querySelector(".down-btn");
+let activeSlideIndx = 0;
+
+// Configure for slide from up to down
+rightSlide.style.top = `-${(slideRightLength - 1) * sliderHeight}px`;
+
+upBtn.addEventListener("click", function () {
+  slider("up");
+});
+downBtn.addEventListener("click", function () {
+  slider("down");
+});
+
+function slider(direction) {
+  if (direction === "up") {
+    activeSlideIndx++;
+
+    if (activeSlideIndx > slideRightLength - 1) {
+      activeSlideIndx = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndx--;
+
+    if (activeSlideIndx < 0) {
+      activeSlideIndx = slideRightLength - 1;
+    }
+  }
+
+  leftSlide.style.transform = `translateY(-${
+    activeSlideIndx * sliderHeight
+  }px)`;
+  rightSlide.style.transform = `translateY(${
+    activeSlideIndx * sliderHeight
+  }px)`;
 }
